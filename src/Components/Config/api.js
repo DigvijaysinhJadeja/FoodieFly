66073513a2c95c2158babb38d10b3dbe77f716c0
DigvijaysinhJadeja <digvijysinhjadeja105@gmail.com>
2025-04-24 18:23:11 +0530

    once again

--- src/Components/Config/api.js
+++ src/Components/Config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = "https://foodiefly-server-production.up.railway.app/"
+export const API_URL = "https://foodiefly-server-production.up.railway.app"
 // export const 
 
 export const api = axios.create({
